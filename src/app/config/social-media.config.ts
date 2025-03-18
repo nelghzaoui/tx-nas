3fb81313bee5dd498c7a09e0abb0658ce08c393a
Nasreddine <eln@outlook.be>
2025-03-18 10:10:35 +0100

    add config

---
+++ src/app/config/social-media.config.ts
@@ -0,0 +1,28 @@
+export const socials: SocialMedia[] = [
+  {
+    label: 'Check my Github profile',
+    icon: 'icon-github.svg',
+    link: ''
+  },
+  {
+    label: 'Check my Frontend Mentor profile',
+    icon: 'icon-frontend-mentor.svg',
+    link: ''
+  },
+  {
+    label: 'Check my LinkedIn profile',
+    icon: 'icon-linkedin.svg',
+    link: ''
+  },
+  {
+    label: 'Check my X profile',
+    icon: 'icon-twitter.svg',
+    link: ''
+  }
+];
+
+interface SocialMedia {
+  label: string;
+  icon: string;
+  link: string;
+}
