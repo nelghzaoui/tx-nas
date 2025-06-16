7c6e122f0838fe5c41a6194db47476a5ea319a40
Nasreddine <eln@outlook.be>
2025-06-16 18:31:39 +0200

    change button

--- src/app/components/button.component.ts
+++ src/app/components/button.component.ts
@@ -5,7 +5,7 @@ import { Component, input } from '@angular/core';
   template: `
     <a
       class="
-        text-base font-medium tracking-[2.29px] leading-[1.625rem] uppercase 
+        text-xl font-medium tracking-[2.29px] leading-[1.625rem] uppercase 
         border-b-2 border-b-[#4EE1A0] w-fit m-auto pb-2.5 no-underline bg-transparent 
         text-white duration-200 ease cursor-pointer md:font-bold hover:text-[#4EE1A0] hover:text-xl hover:border-b-[4px]"
       (click)="scrollToTarget($event)"
